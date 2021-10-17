072e58ee290796e68df9063ed0c6dfe4aa5de7ff
Vijay A <vraravam@users.noreply.github.com>
2021-10-17 20:15:19 +0530

    removed 'packages' folder

--- packages/forms/lib/error/styles.d.ts
+++
@@ -1,9 +0,0 @@
-import { Theme } from '../../../theme';
-declare const _default: (theme: Theme) => {
-    message: {
-        color: string;
-        margin: string;
-        fontSize: number;
-    };
-};
-export default _default;
--- packages/forms/lib/input/scorePassword.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function scorePasswordFunc(password: string): number;
